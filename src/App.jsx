db5dd5017366accabd62edfe17cece9e9dea1c7e
CyNyxy <94080207+CyNyxy@users.noreply.github.com>
2023-04-20 05:01:46 +0200

    Fixed app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+
+  return (
+    <div className="App">
+      <Button
+        color={"default"}
+        variant={"default"}
+        size={"lg"}
+        iconStart={"add"}
+        label={"Button"}
+        disabled
+      />
+    </div>
+  )
+}
+
+export default App
